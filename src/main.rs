307d9735b844aa8d504a4cd6941ae125f26e559f
gates <1053302852@qq.com>
2022-04-08 13:17:06 +0800

    完成insert_at_head

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,53 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Debug)]
 struct List<T> {
-    head: Option<Node<T>>,
+    head: Option<Box<Node<T>>>,
     len: u32,
 }
-
+#[derive(Debug)]
 struct Node<T> {
     value: T,
-    next: Box<Node<T>>,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(value: T) -> Node<T> {
+        Node { value, next: None }
+    }
 }
 
 impl<T> List<T> {
-    fn insert_at_head() {}
-    fn insert_at_tail() {}
+    fn new() -> List<T> {
+        List { head: None, len: 0 }
+    }
+    fn insert_at_head(mut self, value: T) -> List<T> {
+        let mut node = Node::new(value);
+        node.next = self.head;
+        self.head = Some(Box::new(node));
+        self.len += 1;
+        self
+    }
+    fn insert_at_tail(&mut self, value: T) {}
     fn insert_at_ith() {}
     fn delete_head() {}
     fn delete_tail() {}
     fn delete_ith() {}
     fn get() {}
     fn get_ith() {}
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert_at_head_test() {
+        let list: List<u32> = List::new();
+
+        let list = list.insert_at_head(1);
+        assert!(list.head.unwrap().value == 1);
+        assert!(list.len == 1);
+    }
 }
